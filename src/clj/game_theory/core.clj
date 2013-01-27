97710c55cf73680f9b3f823e7f5abbc03904cd56
Dan Hammer <dan.s.hammer@gmail.com>
2013-01-27 08:34:10 -0800

    strategies changed to actions

--- src/clj/game_theory/core.clj
+++ src/clj/game_theory/core.clj
@@ -4,11 +4,11 @@
 
 (defn utility-table
   "Given the elements in a game, appropriately structured, returns a
-  map with the actions of each player as the key, and the associate
+  map with the actions of each player as the key, and the associated
   payoffs to the corresponding player as the value."
   [& elements]
-  (let [create-map (fn [m [strategies utilities]]
-                     (assoc m (vec strategies) utilities))]
+  (let [create-map (fn [m [actions utils]]
+                     (assoc m (vec actions) utils))]
     (reduce create-map {} elements)))
 
 (defn utility-fn 
@@ -22,63 +22,66 @@
   the players in that situation."
   [& elements]
   (let [u-table (apply utility-table elements)]
-    (fn [player strategies]
-      (nth (get u-table (vec strategies)) player))))
+    (fn [player actions]
+      (nth (get u-table (vec actions)) player))))
 
 (defn game
-   "Returns a game from the number of players, the strategies for each
+  "returns a game from the number of players, the strategies for each
    player, and the elements that define the utility function."
-   [p s & elem]
-   {:players (range p)
-   :strategies s
-   :utility-fn (apply build-utility-fn elem)})
+  [N A & game-elements]
+  {:players (range N)
+   :actions A
+   :utility-fn (apply build-utility-fn game-elements)})
 
 (defn players 
-	"returns the number of players in the game."
-	[game]
-   (:players game))
+  "returns the number of players in the game."
+  [game]
+  (:players game))
 
-(defn strategies 
-   "returns the player strategies in the game. If a player parameter is
+(defn actions 
+  "returns the player strategies in the game. If a player parameter is
    provided (index of the player, zero-started) then only the
    strategies for the specified player will be returned."
-   ([game] (:strategies game))
-   ([game player] (nth (strategies game) player)))
+  ([game] (:actions game))
+  ([game player] (nth (actions game) player)))
 
 (defn utility
   "returns the utility value of a game for a player and a given startegy
   vector. The strategies are expected to be a sequence of all the
   strategies that all players are following."
-  [game player strategies]
-  ((:utility-fn game) player strategies))
+  [game player actions]
+  ((:utility-fn game) player actions))
 
-(defn strategy-space 
-	"returns a sequence of all the strategy combinations possible in a
-	given game."
-   [game]
-   (apply combo/cartesian-product (strategies game)))
+(defn action-space
+  "returns a sequence of all the action combinations or action
+	profiles possible in a given game."
+  [game]
+  (apply combo/cartesian-product (actions game)))
 
 (defn iff 
-  "Computes the 'a if and only if b' boolean value."
+  "returns the 'a if and only if b' boolean value."
   [a b]
   (or (and a b)
       (and (not a) (not b))))
 
-(defn single?
+(defn one?
   "returns true if the supplied column is of length one"
   [coll]
   (= (count coll) 1))
 
 (defn only-change?
-  "accepts a player index and a new and old action profile; returns
+  "accepts a player index, and new and old action profiles; returns
   true if and only if the supplied player's action is the only one to
   change."
   [player a-old a-new]
-  (let [bool-vec  (map = a-old a-new)
-        false-vec (filter false? bool-vec)]
-    (if (single? false-vec)
+  (let [false-vec (filter false? (map = a-old a-new))]
+    (if (one? false-vec)
       (not (nth false-vec player))
       false)))
+
+(defn action-dominance
+  [comparator game action player]
+  (let [player-util (fn [x] (utility game player x))]))
 
 (def prisoners-dilemma
   (game 2 [[:confess :deny] [:confess :deny]]
