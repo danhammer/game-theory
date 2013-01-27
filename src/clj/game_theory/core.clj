77655702ac44c956669435ee036969e383f854fd
Dan Hammer <dan.s.hammer@gmail.com>
2013-01-27 10:23:03 -0800

    first working namespace, with two tests

--- src/clj/game_theory/core.clj
+++ src/clj/game_theory/core.clj
@@ -1,7 +1,6 @@
 (ns game-theory.core
   (:require [clojure.contrib.combinatorics :as combo]))
 
-
 (defn utility-table
   "Given the elements in a game, appropriately structured, returns a
   map with the actions of each player as the key, and the associated
@@ -31,7 +30,7 @@
   [N A & game-elements]
   {:players (range N)
    :actions A
-   :utility-fn (apply build-utility-fn game-elements)})
+   :utility-fn (apply utility-fn game-elements)})
 
 (defn players 
   "returns the number of players in the game."
@@ -72,20 +71,72 @@
 (defn only-change?
   "accepts a player index, and new and old action profiles; returns
   true if and only if the supplied player's action is the only one to
-  change."
+  change.  The function is useful in identifying an optimal action
+  profile, conditional on the actions of all other players remaining
+  the same."
   [player a-old a-new]
-  (let [false-vec (filter false? (map = a-old a-new))]
+  (let [bool-vec (map = a-old a-new)
+        false-vec (filter false? bool-vec)]
     (if (one? false-vec)
-      (not (nth false-vec player))
+      (not (nth bool-vec player))
       false)))
 
-(defn action-dominance
-  [comparator game action player]
-  (let [player-util (fn [x] (utility game player x))]))
+(defn limited-profiles
+  "accepts a game, player index, and the action of that player;
+  returns all action profiles such that the player plays the specified
+  action"
+  [game player action])
 
-(def prisoners-dilemma
-  (game 2 [[:confess :deny] [:confess :deny]]
-        [[:confess :confess] [1, 1]]
-        [[:confess :deny]    [4, 0]]
-        [[:deny :confess]    [0, 4]]
-        [[:deny :deny]       [3, 3]]))
+(defn recursive-true?
+  "accepts a nested collection where every element, no matter what
+  level of nesting, is a boolean; returns true iff all elements are
+  true."
+  [nested-coll]
+  (every? true? (flatten nested-coll)))
+
+(defn action-dominance?
+  "accepts a comparator (> or >=), a game, a player, and an action of
+  that player. returns true if that player's action dominates all
+  other possible actions in that game.  Note that the first list
+  comprehension limits the space to the action profiles with the
+  specified player's specified action; the second list comprehension
+  takes the action profiles from the first list comprehension and
+  finds the alternative actions of the specified players.  Then,
+  finally, the outcome utilities associated with the player's action
+  and the player's alternative action are compared.  If the specified
+  player's action always yields a higher utility, then it is
+  dominant."
+  [comparator game player action]
+  (let [aspace (action-space game)
+        util (fn [x] (utility game player x))
+        spec-action? (fn [a] (= (nth a player) action))]
+    (recursive-true?
+     (for [ai (filter spec-action? aspace)]
+       (for [alt-ai (filter (partial only-change? player ai) aspace)]
+         (comparator (util ai) (util alt-ai)))))))
+
+(defn best-response?
+  "accepts a game, player, and action profile across all players.
+  returns true if the player's action in the supplied action profile
+  is weakly preferred to all other possible player actions,
+  conditional on the other players' actions remaining the
+  same (i.e. a_{-i})"
+  [game player action-profile]
+  (let [util (fn [x] (utility game player x))]
+    (recursive-true?
+     (for [a (action-space game) :when (only-change? player action-profile a)]
+       (>= (util action-profile) (util a))))))
+
+(defn nash-equilibrium? 
+  "returns true of the supplied action profile represents a nash
+  equilibrium for the supplied game."
+  [game action-profile]
+  (let [p-set (players game)]
+    (every? true?
+            (map #(best-response? game % action-profile) p-set))))
+
+(defn nash-equilibria 
+	"returns all nash equilibria for the supplied game"
+	[game]
+   (filter (partial nash-equilibrium? game)
+           (action-space game)))
--- test/game_theory/core_test.clj
+++ test/game_theory/core_test.clj
@@ -1,5 +1,14 @@
 (ns game-theory.core-test
-  (:use clojure.test
+  (:use [midje sweet]
         game-theory.core))
 
+(def prisoners-dilemma
+  (game 2 [[:confess :deny] [:confess :deny]]
+        [[:confess :confess] [1, 1]]
+        [[:confess :deny]    [4, 0]]
+        [[:deny :confess]    [0, 4]]
+        [[:deny :deny]       [3, 3]]))
 
+(fact
+  (nash-equilibrium? prisoners-dilemma [:deny :deny]) => false
+  (nash-equilibria prisoners-dilemma) => [[:confess :confess]])
